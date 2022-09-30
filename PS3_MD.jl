41bde5351115c73ec0cdc0e1d51ab4d552aae725
MaryDaniels27 <108483643+MaryDaniels27@users.noreply.github.com>
2022-09-29 23:02:10 -0500

    While loop working!
    Finally got a working, accurate while loop! The reason the other loops were giving me the wrong aggregate capital and labor was the fact that I wasn't re-initializing the population distribution before re-solving the model. Thus, it was adding the various distributions to one another and getting an increasing aggregate labor and capital.

--- PS3_MD.jl
+++ PS3_MD.jl
@@ -1,9 +1,10 @@
 ### PS 3 ###
+using Parameters, Plots #import the libraries we want
 
 #creating the capital grid which has more density at lower values
-i_max = 14
+i_max = 75
 i_min = 0
-nk = 180
+nk = 1000
 inc = (i_max - i_min)/(nk - 1).^2  #increment
 aux = collect(1:1:nk)
 assets = zeros(length(aux))
@@ -84,23 +85,27 @@ mutable struct Results
     val_func::Array{Float64, 3} #value function, we need three dimensions because we need a value function for each age and productivity state
     pol_func::Array{Float64, 3} #policy function for assets
     labor::Array{Float64, 3}  #optimal labor supply
-end
-
-mutable struct Results_2
+    
+    #prices, capital, labor
     r::Float64 #interest rate
     w::Float64 #wage rate
     b::Float64  #social security benefit
-    K_0::Float64 #aggregate capital
-    L_0::Float64 #aggregate labor
+    K_0::Float64 #aggregate capital intial guess
+    L_0::Float64 #aggregate labor initial guess
 end
-
 
 function Initialize()
     prim = Primitives() #initialize primtiives
-    val_func = zeros(prim.na, prim.nz, 66) #initial value function guess
-    pol_func = zeros(prim.na, prim.nz, 66) #initial policy function for asset guess
+    val_func = zeros(prim.na, prim.nz, prim.N) #initial value function guess
+    pol_func = zeros(prim.na, prim.nz, prim.N) #initial policy function for asset guess
     labor = zeros(prim.na, prim.nz, prim.tw) #inital guess of the optimal labor supply
-    res = Results(val_func, pol_func, labor) #initialize results struct
+
+    K_0 = 3.64  #initial guess of capital
+    L_0 = 0.43  #initial guess of labor
+    w = 1.05
+    r = 0.05
+    b = 0.2
+    res = Results(val_func, pol_func, labor, r, w, b, K_0, L_0) #initialize results struct
     prim, res #return deliverables
 end
 
@@ -124,27 +129,27 @@ function Initialize_3()
     mass = mu_results(mu, mu_dist, mu1)
 end
 
-#solving for r, w and b
-function Initialize_2()
-    @unpack α, R, δ, θ, N = prim
+
+function Prices(prim::Primitives, res::Results, mass::mu_results)
+    @unpack α, δ, θ, R, N = prim
     @unpack mu = mass
-    K_0 = 3.63  #initial guess of capital
-    L_0 = 0.43  #initial guess of labor
-    #Using the firm's FOC, we can solve for r and w
-    w = (1-α)*L_0^(-α)*K_0^α  #wage rate
-    r_k = α*K_0^(α - 1)*L_0^(1-α)  #the firm's interest rate
-    r = r_k - δ  #the household's interest rate which is the firm's interest rate adjusted for depreciation
+    @unpack K_0, L_0 = res
+    w = (1-α)*(L_0)^(-α)*((K_0)^α)  #wage rate
+    r_k = α*(K_0)^(α - 1)*(L_0)^(1-α)  #the firm's interest rate
     #Using the government budget constraint, we can solve for b
-    b = (θ*w*L_0)/(sum(mu[R:N]))
-    res2 = Results_2(r, w, b, K_0, L_0) #initialize our economy's Parameters
+    b = (θ*w*(L_0))/(sum(mu[R:N]))
+    r = r_k - δ  #the household's interest rate which is the firm's interest rate adjusted for depreciation
+    res.w = w
+    res.r = r
+    res.b = b
 end
+
 
 #Solving the dynamic programming problem of retirees and workers
 
-function Bellman(prim::Primitives, res::Results, res2::Results_2)
+function Bellman(prim::Primitives, res::Results)
     @unpack N, R, tw, θ, γ, σ, η, z, markov, β, a_grid, na, nz = prim
-    @unpack val_func = res
-    @unpack r, w, b = res2
+    @unpack val_func, r, w, b = res
     
     #Solving the problem of retirees
     for a_index = 1:na, z_index = 1:nz #looping over assets and states
@@ -157,11 +162,11 @@ function Bellman(prim::Primitives, res::Results, res2::Results_2)
         for a_index = 1:na, z_index = 1:nz  #looping over assets today
             a = a_grid[a_index] #setting the value of a
             candidate_max = -Inf  #initial guess of candidate max
-            budget = (1 + r)a + b #calculate the budget
+            budget = (1 + r)*a + b #calculate the budget
             for ap_index = 1:na #looping over assets tomorrow
                 c = budget - a_grid[ap_index] #consumption given a' selection
                 if c>0  #check for positivity
-                    val = (c^((1-σ)γ))/(1-σ) + β * res.val_func[ap_index, z_index, (j+1)] #calculate the value function while looking at next period's value function
+                    val = (c^((1-σ)*γ))/(1-σ) + β * res.val_func[ap_index, z_index, j+1] #calculate the value function while looking at next period's value function
                     if val > candidate_max
                         candidate_max = val  #if our value function association with a' is greater than the current candidate max, update candidate max 
                         #to be equal to val. Then repeat the process for the next value of a' and update candidate max if this value of a' yields a higher
@@ -180,8 +185,9 @@ function Bellman(prim::Primitives, res::Results, res2::Results_2)
         for a_index = 1:na  #looping over assets today
             a = a_grid[a_index] #setting the value of a
             for z_index = 1:nz #looping for productivity states
+                candidate_max = -Inf  #initial guess of candidate max
                 for ap_index = 1:na #looping over assets tomorrow
-                    l = (γ*(1-θ)*(z[z_index] * η[i])*w - (1-γ)*((1+r)*a_grid[a_index] - a_grid[ap_index]))./((1-θ)*w*(z[z_index] * η[i]))  #for a given combination (z, a, a'), this is the 
+                    l = (γ*(1-θ)*(z[z_index] * η[i])*w - (1-γ)*((1+r)*a_grid[a_index] - a_grid[ap_index]))/((1-θ)*w*(z[z_index] * η[i]))  #for a given combination (z, a, a'), this is the 
                     #optimal labor supply
                     if l > 1  #this if, else loop ensures that labor supply is bounded between 0 and 1
                         l = 1
@@ -189,10 +195,9 @@ function Bellman(prim::Primitives, res::Results, res2::Results_2)
                         l = 0
                     end
                     budget = w*(1-θ)*(z[z_index] * η[i])*l + (1 + r)*a  #calculate the budget
-                    candidate_max = -Inf  #initial guess of candidate max
                     c = budget - a_grid[ap_index] #consumption given a' selection
                         if c>0  #check for positivity
-                            val = ((c^γ * (1-l)^(1-γ))^(1-σ))/(1-σ) + β * sum(res.val_func[ap_index, :, i+1].* markov[z_index, :]) #calculate the value 
+                            val = (((c^γ)*((1-l)^(1-γ)))^(1-σ))/(1-σ) + β * sum(res.val_func[ap_index, :, i+1].* markov[z_index, :]) #calculate the value 
                             #function while looking at next period's value function
                                 if val > candidate_max
                                     candidate_max = val  #update candidate max
@@ -207,8 +212,139 @@ function Bellman(prim::Primitives, res::Results, res2::Results_2)
     end
 end
 
+function Distribution(prim::Primitives, res::Results, mass::mu_results)
+    @unpack N, R, tw, z, a_grid, na, nz, markov = prim
+    @unpack pol_func = res
+    @unpack mu, mu_dist, mu1 = mass
 
+    mu_dist[1, 1, 1] = 0.2037  #initial mass of high productivity people
+    mu_dist[1, 2, 1] = 0.7963   #initial mass of low productivity people
+    for j = 2:prim.N  #loopig over the ages
+        for ap = 1:prim.na  #looping over assets tomorrow
+            for z = 1:2  #looping over z from today
+                d = findall(x->x == a_grid[ap], res.pol_func[:, z, j-1])
+                    for i = 1:length(d) #loop over the indices you find for a specific z
+                        for zp = 1:2  #loop over zp 
+                            mu_dist[ap, zp, j] = mu_dist[ap, zp, j] + mu_dist[d[i], z, j-1] .* markov[z, zp]
+                        end
+                    end
+            end
+        end
+    end
 
+    #The final step is weighting each generation according to the relative sizes calculated by mu.
+    for i = 1:prim.N
+        mu1[:, :, i] = mu_dist[:, :, i] .* mu[i]
+    end
+    mass.mu_dist = mu_dist  #updating the unweighted distribution
+    mass.mu1 = mu1  #updating the weighted distribution 
+end
+
+###  Market Clearing  ###
+
+#Calculating aggregate capital
+#1. for each generation, sum the two states together in the distribution
+#2. multiply the agggregate distribution for each asset by the asset holding
+function MarketClearing(prim::Primitives, res::Results, mass::mu_results)
+    @unpack labor = res
+    @unpack a_grid, N, na, nz, tw, z, η = prim
+    @unpack mu1 = mass
+
+    agg_dist = zeros(prim.na, prim.N) #In this array each column corresponds to a different generation. Each row is contains the result of summing the distribution at a given
+    #asset holding level then multiplying that by the asset holding amount. 
+    #3. sum all the generations together to get aggregate capital
+    for i = 1:prim.N
+        for j = 1:prim.na
+        agg_dist[j, i] = sum(mu1[j, :, i]) .* a_grid[j]
+        end
+    end
+    agg_capital = sum(agg_dist)
+
+    #Calculating aggregate labor 
+    #begin by calculating e(z, η_j) as vector
+    e = zeros(prim.tw, prim.nz)
+    for i = 1:prim.tw
+        for j = 1:prim.nz
+            e[i, j] = prim.z[j] * prim.η[i]
+        end
+    end
+
+    agg_l = zeros(prim.na, prim.tw)  #each column corresponds to a different generation and each row corresponds to a different asset holding position. The first element agg_l[1,1] is
+    #for generation 1, the sum over productivity states of mu1*e(z, η_j)*labor where this multiplication occurs for each productivity state first then we sum.
+    for i = 1:prim.tw  #pick a generation
+        for a = 1:prim.na  #pick an asset holding
+            for j = 1:prim.nz  #pick a productivity state
+                agg_l[a, i]= sum(mu1[a,:,i].*e[i,:].*labor[a,:,i])
+            end
+        end
+    end
+    agg_labor = sum(agg_l)
+
+    return agg = [agg_capital, agg_labor]
+end
+
+function Solve_model(prim::Primitives, res::Results, mass::mu_results)
+    Prices(prim, res, mass)   #Define prices by solving the firm's problem and GBC
+    println("Capital is", res.K_0)
+    println("Labor is", res.L_0)
+    Bellman(prim, res) #solve the Bellman!
+    Distribution(prim, res, mass)   #solve for the stationary distribution
+    MarketClearing(prim, res, mass)
+    K_1 = MarketClearing(prim,res,mass)[1]
+    L_1 = MarketClearing(prim,res,mass)[2]
+    return K_1, L_1
+end
+
+prim, res = Initialize()
+mass = Initialize_3()
+
+##Now we will update the prices to try to get a better aggregate capital and aggregate labor 
+function Iterate(prim::Primitives, res::Results, mass::mu_results; tol::Float64 = 0.001)
+    @unpack K_0, L_0 = res  #Unpack initial guess of K and L
+    prim, res = Initialize()
+    mass = Initialize_3()
+    Solve_model(prim, res, mass)   #solve the model with the initial guess
+    #Solving aggregate capital and labor
+    K_1 = MarketClearing(prim,res,mass)[1]  #define the new aggregate capital which is found from market clearing
+    L_1 = MarketClearing(prim,res,mass)[2]   #new aggregate labor from market clearing
+
+    println("Aggregate capital initially", K_1)
+    println("Aggregate labor initially", L_1)
+
+    #calculate the errors for capital and labor separately
+    err_k = abs(K_1 - res.K_0)
+    err_l = abs(L_1 - res.L_0)
+    n = 0  #counter
+    
+    while err_k > tol || err_l > tol
+        println("on iteration", n)
+        if abs(err_k) > tol
+            res.K_0 = 0.9res.K_0 + 0.1K_1
+        end
+
+        if abs(err_l) > tol
+            res.L_0 = 0.9res.L_0 + 0.1L_1
+        end
+
+        #After updating the initial guess of K and L, we now want to re-run the model
+        mass = Initialize_3()   #re-initialize the population distribution
+        Solve_model(prim, res, mass)
+        K_1 = MarketClearing(prim,res,mass)[1] 
+        L_1 = MarketClearing(prim,res,mass)[2]
+
+        #update the errors
+        err_k = abs(K_1 - res.K_0)
+        err_l = abs(L_1 - res.L_0)
+
+        n+=1
+        println("Aggregate capital now", K_1)
+        println("Aggregate labor now", L_1)
+        
+    end  #end while loop
+end #end iteration function 
+
+Iterate(prim, res, mass)
+##Converged in 51 iterations. The aggregate capital is 3.364317 and the aggregate labor is 0.3432
 
 
 
--- PS3_compute_MD.jl
+++ PS3_compute_MD.jl
@@ -6,7 +6,8 @@ prim, res = Initialize() #initialize primitive and results structs
 mass = Initialize_3()    #initialize the population distribution
 res2 = Initialize_2()    #initialize r, w, b
 @elapsed Bellman(prim, res, res2) #solve the Bellman!
-include("PS3_distribution_MD.jl")  #solve for the stationary distribution
+@elapsed Distribution()  #solve for the stationary distribution
+MarketClearing(prim, res, mass)
 @unpack val_func, pol_func, labor = res
 @unpack a_grid = prim
 @unpack mu, mu_dist, mu1 = mass
@@ -19,8 +20,104 @@ Plots.plot(a_grid, val_func[:, :, 50], title="Value Function", label = ["high pr
 #graphing the savings function
 Plots.plot(a_grid, pol_func[:, :, 20], title="Savings Function", label = ["high productivity" "low productivity"])
 
+#just checking the population
+Plots.plot(a_grid, mu1[:, :, 65], title = "Population Distribution at J = 65")
 
-Plots.plot(a_grid, mu_dist[:, :, 65], title = "Population Distribution at J = 65")
+
+
+MarketClearing(prim, res, mass)
+K_1 = MarketClearing(prim,res,mass)[1]
+L_1 = MarketClearing(prim,res,mass)[2]
+
+#aggregate capital is 3.9967 which is a little too high
+#aggregate labor is 0.325 which is too low
+
+
+function Solve(prim::Primitives, res::Results, res2::Results_2, mass::mu_results)
+    Prices(prim, res2)   #Define prices by solving the firm's problem and GBC
+    println("Capital is", res2.K_0)
+    println("Labor is", res2.L_0)
+    Bellman(prim, res, res2) #solve the Bellman!
+    Distribution()   #solve for the stationary distribution
+    MarketClearing(prim, res, mass)
+    K_1 = MarketClearing(prim,res,mass)[1]
+    L_1 = MarketClearing(prim,res,mass)[2]
+    return K_1, L_1
+end
+
+Solve(prim, res, res2, mass)
+
+
+
+##Now we will update the prices to try to get a better aggregate capital and aggregate labor 
+function Iterate(prim::Primitives, res::Results, res2::Results_2, mass::mu_results; tol::Float64 = 0.001)
+    @unpack K_0, L_0 = res2  #Unpack initial guess of K and L
+    Solve(prim, res, res2, mass)   #solve the model with the initial guess
+
+
+    #Solving aggregate capital and labor
+    K_1 = MarketClearing(prim,res,mass)[1]  #define the new aggregate capital which is found from market clearing
+    L_1 = MarketClearing(prim,res,mass)[2]   #new aggregate labor from market clearing
+
+    println("Aggregate capital initially", K_1)
+    println("Aggregate labor initially", L_1)
+
+    #calculate the errors for capital and labor separately
+    err_k = abs(K_1 - res2.K_0)
+    err_l = abs(L_1 - res2.L_0)
+    n = 0  #counter
+    
+    while err_k > tol || err_l > tol
+        println("on iteration", n)
+        if abs(err_k) > tol
+            res2.K_0 = 0.9res2.K_0 + 0.1K_1
+        end
+
+        if abs(err_l) > tol
+            res2.L_0 = 0.9res2.L_0 + 0.1L_1
+        end
+
+        #After updating the initial guess of K and L, we now want to re-run the model
+        Solve(prim, res, res2, mass)
+        K_1 = MarketClearing(prim,res,mass)[1] 
+        L_1 = MarketClearing(prim,res,mass)[2]
+
+        #update the errors
+        err_k = abs(K_1 - res2.K_0)
+        err_l = abs(L_1 - res2.L_0)
+
+        n+=1
+        println("Aggregate capital now", K_1)
+        println("Aggregate labor now", L_1)
+        
+    end  #end while loop
+end #end iteration function 
+
+Iterate(prim, res, res2, mass)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 println("All done!")
 ################################
